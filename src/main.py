d94026f67126506a25157da23ff277cccbd7cd1d
Michael Matthews <mmatthews06@gmail.com>
2019-09-28 15:07:30 -0400

    add a run method and test wether main calls it. Also refactor existing tests to use variable params

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ class ExampleClass(object):
         self.foo = foo
         self.bar = bar
 
+    def run(self, param):
+        pass
+
 
 def parse_args():
     import argparse
@@ -12,12 +15,17 @@ def parse_args():
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
     parser.add_argument('-f', '--foo')
     parser.add_argument('-b', '--bar')
+    parser.add_argument('-r', '--run', action='store_true', default=False)
+    parser.add_argument('-p', '--param', default='not specified!')
 
     return parser.parse_args()
 
 def main():
     args = parse_args()
     example = ExampleClass(args.foo, args.bar)
+
+    if args.run:
+        example.run(args.param)
 
 
 if __name__ == '__main__':
