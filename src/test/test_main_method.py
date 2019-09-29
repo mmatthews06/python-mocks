bdb54b461fef2dd37741e9189d9a872a5f7df180
Michael Matthews <mmatthews06@gmail.com>
2019-09-29 19:27:58 -0400

    Add a test for if __name__ (for demo purposes), and document some tests

--- src/test/test_main_method.py
+++ src/test/test_main_method.py
@@ -1,4 +1,5 @@
 
+import inspect
 import sys
 import unittest
 from io import StringIO
@@ -12,6 +13,9 @@ class TestMainMethod(unittest.TestCase):
 
     @patch('main.ExampleClass')
     def test_main_passes_foo(self, MockExampleClass: MagicMock):
+        '''
+        Test is main passes the foo argument.
+        '''
         foo = 'hello'
         args = ['main', '-f', foo]
         with  patch.object(sys, 'argv', args):
@@ -20,6 +24,9 @@ class TestMainMethod(unittest.TestCase):
 
     @patch('main.ExampleClass')
     def test_main_passes_bar(self, MockExampleClass: MagicMock):
+        '''
+        Test is main passes the bar argument.
+        '''
         bar = 'bye'
         args = ['main', '-b', 'bye']
         with  patch.object(sys, 'argv', args):
@@ -28,6 +35,9 @@ class TestMainMethod(unittest.TestCase):
 
     @patch('main.ExampleClass')
     def test_main_passes_foo_and_bar(self, MockExampleClass: MagicMock):
+        '''
+        Test is main passes both arguments when provided (NOTE: not likely necessary)
+        '''
         foo = 'hello'
         bar = 'bye'
         args = ['main', '-f', foo, '-b', bar]
@@ -37,22 +47,45 @@ class TestMainMethod(unittest.TestCase):
 
     @patch('sys.argv', ['main', '-r'])
     @patch('main.ExampleClass.run')
-    def test_main_runs_method(self, MockRunMethod: MagicMock):
+    def test_main_runs_method(self, mockRunMethod: MagicMock):
+        '''
+        Test if main calls the run method when `-r` is provided.
+        '''
         defaultRunParam = 'not specified!'
         main()
-        MockRunMethod.assert_called_with(defaultRunParam)
+        mockRunMethod.assert_called_with(defaultRunParam)
 
     @patch('main.ExampleClass.run')
-    def test_main_runs_method_with_param(self, MockRunMethod: MagicMock):
+    def test_main_runs_method_with_param(self, mockRunMethod: MagicMock):
+        '''
+        Test is main passes the right parameter to the run method
+        '''
         runParam = 'I am a run param!'
         args = ['main', '-r', '-p', runParam]
         with  patch.object(sys, 'argv', args):
             main()
-            MockRunMethod.assert_called_with(runParam)
+            mockRunMethod.assert_called_with(runParam)
 
     @patch('sys.argv', ['main', '-v'])
     @patch('sys.stdout', new_callable=StringIO)
     def test_display_version(self, mockStdOut: StringIO):
+        '''
+        Test printing the version. Also demos expecting an assertion.
+        (NOTE: possibly not needed, but nice to have.)
+        '''
         with self.assertRaises(SystemExit):
             main()
         self.assertIn('main 1.0', mockStdOut.getvalue())
+
+    def test_if_name_equals_main(self):
+        '''
+        Test if the last lines are the canonical ones.
+        (NOTE: I do not personally endorse this test, reasonable minds may differ.)
+        '''
+        import main as mainModule
+        sourceLines = inspect.getsourcelines(mainModule)[0]
+        penultimateLine = sourceLines[-2]
+        lastLine = sourceLines[-1]
+        self.assertEqual(penultimateLine, "if __name__ == '__main__':\n")
+        self.assertEqual(lastLine, "    main()\n")
+
