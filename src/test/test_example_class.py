dfead8ee4f7bd4a3f8fe8cd2c0fa1179cd0fb184
Michael Matthews <mmatthews06@gmail.com>
2019-09-29 20:36:46 -0400

    cleanup the last linting issues

--- src/test/test_example_class.py
+++ src/test/test_example_class.py
@@ -1,31 +1,37 @@
-
+'''
+test_example_class.py
+Author: Michael Matthews
+'''
+import subprocess
 import unittest
-from io import StringIO
 from unittest.mock import patch
 from unittest.mock import MagicMock
 
 from main import ExampleClass
 
 
-class TestMainMethod(unittest.TestCase):
+class TestExampleClass(unittest.TestCase):
+    '''
+    Test for the ExampleClass.
+    '''
     def setUp(self):
         self.loggerPatch = patch('main.logger')
         self.mockLogger: MagicMock = self.loggerPatch.start()
 
-    @patch('subprocess.Popen')
-    def test_run_logging(self, mockPopen: MagicMock):
+    def test_run_logging(self): # pylint: disable=invalid-name
         '''
         Test simple logging.
         '''
         runParam = 'foo'
         ex = ExampleClass('hello', 'bye')
-        ex.run(runParam)
+        with patch.object(subprocess, 'Popen'):
+            ex.run(runParam)
 
         self.mockLogger.info.assert_any_call(f'Info message - {runParam}!')
         self.assertIn(runParam, self.mockLogger.info.call_args_list[0][0][0])
 
     @patch('subprocess.Popen.__enter__')
-    def test_subprocess_out(self, mockPopen: MagicMock):
+    def test_subprocess_out(self, mockPopen: MagicMock): # pylint: disable=invalid-name
         '''
         Test logging of Popen output
         '''
@@ -38,7 +44,7 @@ class TestMainMethod(unittest.TestCase):
         self.mockLogger.info.assert_any_call(f'Out - {"".join(commandStdOut)}')
 
     @patch('subprocess.Popen')
-    def test_command_build(self, mockPopen: MagicMock):
+    def test_command_build(self, mockPopen: MagicMock): # pylint: disable=invalid-name
         '''
         Test whether we built the command submitted to Popen correctly.
         '''
